5f720293753d531f288f88147e5a2c1115085fb0
ow22vira <ow22vira@login01.sc.uni-leipzig.de>
2026-06-12 14:40:07 +0200

    fix run script, add gitignore

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=24G                    
 #SBATCH --gres=gpu:1                 
-#SBATCH --time=2-00:00:00              # Maximale Laufzeit anpassen
+#SBATCH --time=0-02:00:00              # Maximale Laufzeit anpassen
 #SBATCH --output=logs/llm_job_%j.log      # logs
 
-source ~/.venv/bin/activate
+source .venv/bin/activate
 
-pip3 install -r requirements.txt
+pip3 install vllm
 
 python src/llm_parser/llm_parser.py \
     > logs/output_quwen.log 2>&1
