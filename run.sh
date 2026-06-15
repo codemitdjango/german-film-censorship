8d827d3b5d89616c49c7e67c9881820a0ff7e981
marisa <marisa.weiss@gmx.de>
2026-06-15 14:28:12 +0200

    ich geh crash out

--- run.sh
+++ run.sh
@@ -9,17 +9,24 @@
 #SBATCH --time=0-02:00:00                   # Maximale Laufzeit anpassen
 #SBATCH --output=logs/qwen_parser%j.log     # logs
 
-source .venv/bin/activate
-
-#pip3 install vllm
+module purge
+module load GCCcore/15.2.0  # Passend zu deiner Python-Installation
+module load Python/3.14.2
+module load CUDA/12.4.0
 
 # Optional: Stelle sicher, dass du im richtigen Verzeichnis bist
 cd $SLURM_SUBMIT_DIR
 
-# NCCL Fixes & Debugging??
-export NCCL_DEBUG=INFO
-# export NCCL_IB_DISABLE=1
+source .venv/bin/activate
+
+# 4. Umgebungsvariablen für vLLM / PyTorch Distributed (Bugfixes)
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
+export NCCL_DEBUG=WARN # INFO spammt dein Log mit über 100 Zeilen pro GPU voll, WARN reicht.
+
+# Verhindert den IPv6 / localhost Error (errno: 97), den du im Log hast:
+export VLLM_HOST_IP=127.0.0.1
+export NCCL_SOCKET_IFNAME=eth0 # Oder den entsprechenden Infiniband/Ethernet-Namen des Paula-Knotens, z.B. ibs5
+
 
 python src/llm_parser/llm_parser.py \
     > logs/output_quwen.log 2>&1
